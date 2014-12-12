eb7fe53151f03496289f003ab1b18912ec230ec2
YAMAMOTO Takashi <yamamoto@valinux.co.jp>
2014-12-12 15:26:01 +0900

    adapt for ofagent
    also, remove a bashism

--- ofagent-split.sh
+++ ofagent-split.sh
@@ -21,33 +21,9 @@ mkdir -p $tmpdir
 logfile=$tmpdir/output.log
 echo "Logging to $logfile"
 
-src_repo="$1"
-dst_repo="$2"
-service="$3"
-repo_name="$4"
-module_name="$5"
+dst_repo="$1"
 
-if [ ! -d "$src_repo" ]; then
-    echo "usage: `basename $0` <source-repo-dir> <dest-repo-dir>"
-    exit 1
-elif [ -d "$dst_repo" ]; then
-    echo "ERROR: $dst_repo already exists"
-    exit 1
-fi
-
-# Redirect stdout/stderr to tee to write the log file
-# (borrowed from verbose mode handling in devstack)
-exec 1> >( awk '
-                {
-                    cmd ="date +\"%Y-%m-%d %H:%M:%S \""
-                    cmd | getline now
-                    close("date +\"%Y-%m-%d %H:%M:%S \"")
-                    sub(/^/, now)
-                    print
-                    fflush()
-                }' | tee "$logfile" ) 2>&1
-
-function count_commits {
+count_commits() {
     echo
     echo "Have $(git log --oneline | wc -l) commits"
 }
@@ -57,10 +33,8 @@ set -x
 # The list of files we want to start with.
 
 basedir=`dirname "$0"`
-files_to_keep=$(cat $basedir/{services,$service}-keep.txt)
+files_to_keep=$(cat ./ofagent.txt)
 
-# Get ourselves a play area.
-rsync -a "$src_repo"/ "$dst_repo"
 cd "$dst_repo"
 
 # Pull in feature branches
@@ -99,7 +73,7 @@ done
 # Purge all parents for those commits
 
 set_roots="
-if [ '' $(for root in $roots; do echo " -o \"\$GIT_COMMIT\" == '$root' "; done) ]; then
+if [ '' $(for root in $roots; do echo " -o \"\$GIT_COMMIT\" = '$root' "; done) ]; then
     echo ''
 else
     cat
@@ -130,31 +104,6 @@ git filter-branch \
     --parent-filter "$set_roots" \
     --commit-filter "$skip_empty" \
     -- --all
-
-# Move things around
-echo "Moving files into place..."
-git mv neutron $module_name
-
-echo "Patching base files"
-patch -p1 < ../split-repo/$service.patch
-
-echo "Fixing imports..."
-find $module_name -type f | while read file; do
-    replace "from neutron.services" "from $module_name.services" -- $file
-    replace "import neutron.services" "import $module_name.services" -- $file
-    replace "from neutron.db.firewall" "from neutron_fwaas.db.loadbalancer" -- $file
-    replace "from neutron.db.loadbalancer" "from neutron_lbaas.db.loadbalancer" -- $file
-    replace "from neutron.db.vpn" "from neutron_vpnaas.db.loadbalancer" -- $file
-done
-
-# Pull in oslo-incubator
-echo "Grabbing oslo incubator"
-cd ../oslo-incubator
-./update.sh ../$dst_repo
-cd ../$dst_repo
-
-git add .
-git commit -m "Rename module"
 
 echo "The scratch files and logs from the export are in: $tmpdir"
 echo "The next step is to make the tests work."
